4e501d9f5d48e93d2fe62fcbacb3ea74dd2283a7
roger <roger.van.koughnett@gmail.com>
2012-01-27 12:54:33 -0500

    done???

--- js/list.js
+++ js/list.js
@@ -2,37 +2,33 @@
 
 var data = document.getElementById("task");
 
+document.getElementById("todo-form").addEventListener("submit", function(ev) {
+		ev.preventDefault();	
 
-var nolist = function(ev) {
-	
-  
 	if (data.value == "") {
 			data.className = "textInvalid";
 			alert ("you must put something in the New Task field")
 			data.className = "";
-				} else {
-				createlist();
-				}
-		};
+	} else {
 	
-	var createlist = function(ev) {
+
 			var newtask = document.getElementById("incomplete").appendChild(document.createElement("li"));
 			newtask.innerHTML = data.value;
 			newtask.className = "incomplete";
 			
-			newtask.addEventListener("click", function(ev) {
+				newtask.addEventListener("click", function(ev) {
 	
-				if (ev.target.className == "incomplete") { 
-						ev.target.setAttribute("class", "done"); 
-						document.getElementById("done").appendChild(ev.target);
-				}else{
-						document.getElementById("incomplete").appendChild(ev.target);
-						ev.target.setAttribute("class", "incomplete"); 
+					if (ev.target.className == "incomplete") { 
+							ev.target.setAttribute("class", "done"); 
+							document.getElementById("done").appendChild(ev.target);
+					}else{
+							document.getElementById("incomplete").appendChild(ev.target);
+							ev.target.setAttribute("class", "incomplete"); 
 					};
-			}, false);
-};
+				}, false);
+		
+	};
 
 	data.value = "";	
 
-
-document.getElementById("submit").addEventListener("click", nolist, false);
+}, false);
